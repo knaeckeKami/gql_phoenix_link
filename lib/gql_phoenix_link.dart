c8dab7f0d8cf4805412aa0a6c722969b48bd66f5
Martin Kamleithner <martin+kamleithner@diagnosia.com>
2020-08-14 00:02:35 +0200

    configure library exports

--- lib/gql_phoenix_link.dart
+++ lib/gql_phoenix_link.dart
@@ -1,8 +1,1 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library gql_phoenix_link;
-
 export 'src/phoenix_link.dart';
-
-// TODO: Export any libraries intended for clients of this package.
